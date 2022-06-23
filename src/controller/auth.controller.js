82d6858cb6059dc602fd75b49b4f0d356485fb50
ljl-wanfanjdt <842165957@qq.com>
2022-06-23 23:41:51 +0800

    登录功能提交2

---
+++ src/controller/auth.controller.js
@@ -0,0 +1,24 @@
+/**
+ * @description 授权中间件,主要是给已经登录用户添加令牌token(jwt方式)
+ */
+const jwt = require('jsonwebtoken')
+
+const { PRIVATE_KEY } = require('../conf/config')
+
+class AuthController {
+  async login(ctx, next) {
+    const { id, user_name: userName } = ctx.user
+    console.log(PRIVATE_KEY);
+    const token = jwt.sign({ id, userName }, PRIVATE_KEY, {
+      expiresIn: 60 * 60 * 24,
+      algorithm: 'RS256'
+    });
+    console.log(token);
+    ctx.body = { id, userName, token }
+  }
+
+  async success(ctx, next) {
+    ctx.body = "授权成功~"
+  }
+}
+module.exports = new AuthController()
--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
-const { getUserName } = require('../services/user.service')
-const errorTypes = require('../constants/error-types');
+const { getUserName, getPassword } = require('../services/user.service')
+const errorTypes = require('../constants/error-types')
+const handlePassword = require('../utils/password.encrypt')
 async function verifyUserLogin(ctx, next) {
   const { userName, password } = ctx.request.body
 
@@ -11,6 +12,17 @@ async function verifyUserLogin(ctx, next) {
     const error = new Error(errorTypes.USER_ALREADY_EXISTS);
     return ctx.app.emit('error', error, ctx);
   }
+
+  // 密码校验
+  const toPassword = handlePassword(password)
+  if (toPassword !== user.password) {
+    const error = new Error(errorTypes.PASSWORD_IS_INCORRENT);
+    return ctx.app.emit('error', error, ctx);
+  }
+
+  // 将用户数据挂载到执行上下文对象(走到这一步表示验证通过了,下个中间件开始颁发令牌)
+  ctx.user = user;
+  await next();
 }
 
 module.exports = {
